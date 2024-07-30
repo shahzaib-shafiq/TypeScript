5eb7042b4f02cdf8577a5ed5086107fa9c9ef85e
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-30 12:27:31 +0500

    Defiing Function Return Types

--- functions.ts
+++ functions.ts
@@ -1,8 +1,8 @@
-function addTwo(a: number) {
+function addTwo(a: number): Number {
   return a + 2;
 }
 
-function getUpper(a: string) {
+function getUpper(a: string): String {
   return a.toUpperCase();
 }
 
@@ -13,3 +13,11 @@ UserData("Shahzaib", "shahzaib@gmail.com", true);
 let loginUser = (name: string, email: string, isPaid: boolean = true) => {};
 
 addTwo(5);
+
+const heros = ["A", "B", "C", "D"];
+//const heros = [1, 2, 3];
+heros.map((hero): String => {
+  {
+    return `hero is ${hero}`;
+  }
+});

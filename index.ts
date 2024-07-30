68f6e5401019d2c1a77824afbfe9ae999f4ee54a
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-30 11:06:33 +0500

    Working on Type Anotations

--- index.ts
+++ index.ts
@@ -4,4 +4,15 @@ const sum = (a: number, b: number): number => {
   return a + b;
 };
 
-console.log(sum(3, 4));
+//console.log(sum(3, 4));
+
+let flag: boolean = true;
+let isDone: boolean = false;
+
+function isEven(a: number): boolean {
+  return a % 2 === 0;
+}
+
+//console.log(isEven(4));
+
+// let sum: bigint = 456666666666666666666666666;

55be6b72be4a126de7c69184ac5c0e522503fdbd
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-30 12:17:22 +0500

    Function is TS

---
+++ functions.ts
@@ -0,0 +1,15 @@
+function addTwo(a: number) {
+  return a + 2;
+}
+
+function getUpper(a: string) {
+  return a.toUpperCase();
+}
+
+function UserData(name: string, email: string, isPaid: boolean) {}
+
+UserData("Shahzaib", "shahzaib@gmail.com", true);
+
+let loginUser = (name: string, email: string, isPaid: boolean = true) => {};
+
+addTwo(5);
